1e2b5a1a38b7d6deb03658447d3ef79a85ea9a04
gowtham <gowtham.gthm@gmail.com>
2025-07-23 18:28:17 +0530

    menu bar fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,71 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterOutlet,
+    NzIconModule,
+    NzLayoutModule,
+    NzMenuModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-zorro-sample';
+  isCollapsed = false;
+
+  menus: any[] = [
+    {
+      title: 'Dashboard',
+      icon: 'dashboard',
+      subMenu: [
+        {
+          title: 'Welcome',
+          link: '/welcome',
+        },
+        {
+          title: 'Monitor',
+          link: '',
+        },
+        {
+          title: 'Workplace',
+          link: '',
+        },
+      ],
+    },
+    {
+      title: 'Form',
+      icon: 'form',
+      subMenu: [
+        {
+          title: 'Basic Form',
+          link: '',
+        },
+        {
+          title: 'Ultra Form',
+          link: '',
+        },
+        {
+          title: 'Premium Form',
+          link: '',
+        },
+      ],
+    },
+  ];
+
+  constructor(private router: Router) {}
+
+  getSelected(link: string): boolean {
+    return this.router.url === link;
+  }
+
+
 }

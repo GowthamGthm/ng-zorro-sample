8bb7b4587da3c2cb3588934085a086498005a3f0
gowtham <gowtham.gthm@gmail.com>
2025-07-24 00:40:00 +0530

    changes

--- src/app/pages/table1/table1.component.ts
+++ src/app/pages/table1/table1.component.ts
@@ -1,17 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import type { ColDef } from 'ag-grid-community';
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+interface IRow {
+  make: string;
+  model: string;
+  price: number;
+  electric: boolean;
+}
 
 @Component({
   selector: 'app-table1',
   standalone: true,
-  imports: [],
+  imports: [AgGridAngular],
   templateUrl: './table1.component.html',
-  styleUrl: './table1.component.css'
+  styleUrl: './table1.component.css',
 })
-export class Table1Component  implements OnInit {
-
-  ngOnInit(): void {
-
+export class Table1Component implements OnInit {
+  constructor() {
+    // Register all Community features
   }
 
+  rowData: IRow[] = [
+    { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
+    { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
+    { make: 'Mercedes', model: 'EQA', price: 48890, electric: true },
+    { make: 'Fiat', model: '500', price: 15774, electric: false },
+    { make: 'Nissan', model: 'Juke', price: 20675, electric: false },
+  ];
 
+  // Column Definitions: Defines & controls grid columns.
+  colDefs: ColDef<IRow>[] = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' },
+    { field: 'electric' },
+  ];
+
+  defaultColDef: ColDef = {
+    flex: 1,
+  };
+
+  ngOnInit(): void {}
 }
